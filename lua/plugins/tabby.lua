ad3d5355fe2500dd35af20ca1174d48f71d90c41
MartinVacheron <martinvdu92@gmail.com>
2024-06-16 13:43:44 +0200

    Tabs and status line

--- lua/plugins/tabby.lua
+++ lua/plugins/tabby.lua
@@ -1,11 +1,6 @@
-local function tab_name(tab)
-   return string.gsub(tab,"%[..%]","")
-end
-
-
 local function tab_modified(tab)
     local wins = require("tabby.module.api").get_tab_wins(tab)
-    for i, x in pairs(wins) do
+    for _, x in pairs(wins) do
         if vim.bo[vim.api.nvim_win_get_buf(x)].modified then
             return ""
         end
@@ -35,39 +30,41 @@ local function buffer_name(buf)
     return buf
 end
 
+-- Color scheme
+local palette = require("catppuccin.palettes").get_palette("macchiato")
+
 local theme = {
   fill = 'TabLineFill',
-  -- Also you can do this: fill = { fg='#f2e9de', bg='#907aa9', style='italic' }
   head = 'TabLine',
-  current_tab = 'TabLineSel',
-  inactive_tab = 'TabLineFill',
+  current_tab = { fg = palette.blue, bg = palette.base },
+  inactive_tab = 'TabLine',
   tab = 'TabLine',
-  win = 'TabLine', -- modified
+  win = 'TabLine',
   tail = 'TabLine',
 }
-
--- https://github.com/nanozuki/tabby.nvim/discussions/106
 
 return {
     "nanozuki/tabby.nvim",
     config = function ()
-        require("tabby").setup()
+        require("tabby").setup({
+            option = {
+                theme = theme,
+            },
+        })
 
         require('tabby.tabline').set(function(line)
             return {
-                {
-                    { '  ', hl = theme.head },
-                    line.sep('', theme.head, theme.fill),
-                },
-                margin = ' ',
                 line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
-                    local hl = win.is_current() and theme.current_tab or theme.tab
+                    local hl = win.is_current() and theme.current_tab or theme.inactive_tab
+                    local win_nb = win.id
+                    local bg = theme.head
+
                     return {
-                        line.sep('', hl, theme.fill),
+                        line.sep('', hl, bg, theme.fill),
                         win.file_icon(),
                         buffer_name(win.buf_name()),
                         lsp_diag(win.buf().id),
-                        line.sep('', hl, theme.fill),
+                        line.sep('', hl, theme.head, theme.fill),
                         hl = hl,
                         margin = ' ',
                     }
@@ -76,80 +73,17 @@ return {
                 line.tabs().foreach(function(tab)
                     local hl = tab.is_current() and theme.current_tab or theme.tab
                     return {
-                        line.sep('', hl, theme.fill),
+                        line.sep('', hl, theme.head, theme.fill),
                         tab.number(),
-                        tab_name(tab.name()),
                         tab_modified(tab.id),
-                        line.sep('', hl, theme.fill),
+                        line.sep('', hl, theme.head, theme.fill),
                         hl = hl,
                         margin = ' ',
                     }
                 end),
-                margin = ' ',
-                {
-                    line.sep('', theme.tail, theme.fill),
-                    { '  ', hl = theme.tail },
-                },
-                hl = theme.fill,
+                hl = theme.head,
             }
         end)
     end
 }
-
-
--- local theme = {
---   fill = 'TabLine',
---   -- Also you can do this: fill = { fg='#f2e9de', bg='#907aa9', style='italic' }
---   head = 'TabLine',
---   current_tab = 'TabLineSel',
---   tab = 'TabLine',
---   win = 'TabLine',
---   tail = 'TabLine',
--- }
--- require('tabby').setup({
---   line = function(line)
---     return {
---       {
---         { '  ', hl = theme.head },
---         line.sep('', theme.head, theme.fill),
---       },
---       line.tabs().foreach(function(tab)
---         local hl = tab.is_current() and theme.current_tab or theme.tab
---         return {
---           line.sep('', hl, theme.fill),
---           tab.is_current() and '' or '󰆣',
---           tab.number(),
---           tab.name(),
---           tab.close_btn(''),
---           line.sep('', hl, theme.fill),
---           hl = hl,
---           margin = ' ',
---         }
---       end),
---       line.spacer(),
---       line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
---         return {
---           line.sep('', theme.win, theme.fill),
---           win.is_current() and '' or '',
---           win.buf_name(),
---           line.sep('', theme.win, theme.fill),
---           hl = theme.win,
---           margin = ' ',
---         }
---       end),
---       {
---         line.sep('', theme.tail, theme.fill),
---         { '  ', hl = theme.tail },
---       },
---       hl = theme.fill,
---     }
---   end,
---   -- option = {}, -- setup modules' option,
--- })
-
-
-
-
-
-
 
