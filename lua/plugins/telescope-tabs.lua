7b76b863a1302af4ce10428a93ea1b8e8f2bf97d
MartinVacheron <martinvdu92@gmail.com>
2025-10-14 19:31:08 +0200

    Cleaned telescope config and switched to dropdown theme

--- lua/plugins/telescope-tabs.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "https://github.com/LukasPietzschmann/telescope-tabs",
-    config = function ()
-        vim.keymap.set("n", "tt", ":lua require('telescope-tabs').list_tabs()<CR>")
-    end
-}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,46 +6,51 @@ return {
         tag = "0.1.6",
         dependencies = {
             "nvim-lua/plenary.nvim",
-            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-            {
-                "nvim-telescope/telescope-live-grep-args.nvim",
-                -- This will not install any breaking changes.
-                -- For major updates, this must be adjusted manually.
-                version = "^1.0.0",
-            },
+            { "nvim-telescope/telescope-fzf-native.nvim",     build = "make" },
+            { "nvim-telescope/telescope-live-grep-args.nvim", version = "^1.0.0" },
+            { "nvim-telescope/telescope-ui-select.nvim" },
+            { "LukasPietzschmann/telescope-tabs" },
         },
         config = function()
+            local telescope = require("telescope")
             local actions = require("telescope.actions")
             local lga_actions = require("telescope-live-grep-args.actions")
+            local themes = require("telescope.themes")
 
-            require("telescope").setup({
+            -- Global dropdown theme config
+            local dropdown = themes.get_dropdown({
+                previewer = false,
+                layout_config = { width = 0.8, height = 0.6 },
+            })
+
+            telescope.setup({
                 defaults = {
-                    -- Define custom mappings
+                    layout_strategy = "center",
                     mappings = {
-                        i = { -- Insert mode mappings
+                        i = {
                             ["<A-a>"] = function(prompt_bufnr)
-                                local selected_entry = action_state.get_selected_entry()
-                                local file_path = selected_entry.path or selected_entry.filename
-                                if file_path then
-                                    actions.close(prompt_bufnr)                      -- Close Telescope first
-                                    vim.cmd("let g:splitright_backup = &splitright") -- Backup the current 'splitright' setting
-                                    vim.cmd("set splitright")                        -- Ensure splits open to the right
-                                    vim.cmd("vsplit " .. vim.fn.fnameescape(file_path))
-                                    vim.cmd("let &splitright = g:splitright_backup") -- Restore 'splitright'
+                                local entry = action_state.get_selected_entry()
+                                local path = entry.path or entry.filename
+                                if path then
+                                    actions.close(prompt_bufnr)
+                                    vim.cmd("let g:splitright_backup = &splitright")
+                                    vim.cmd("set splitright")
+                                    vim.cmd("vsplit " .. vim.fn.fnameescape(path))
+                                    vim.cmd("let &splitright = g:splitright_backup")
                                     vim.cmd("unlet g:splitright_backup")
                                 end
                             end,
                             ["<esc>"] = actions.close,
                         },
-                        n = { -- Normal mode mappings
+                        n = {
                             ["<A-a>"] = function(prompt_bufnr)
-                                local selected_entry = action_state.get_selected_entry()
-                                local file_path = selected_entry.path or selected_entry.filename
-                                if file_path then
-                                    actions.close(prompt_bufnr) -- Close Telescope first
+                                local entry = action_state.get_selected_entry()
+                                local path = entry.path or entry.filename
+                                if path then
+                                    actions.close(prompt_bufnr)
                                     vim.cmd("let g:splitright_backup = &splitright")
                                     vim.cmd("set splitright")
-                                    vim.cmd("vsplit " .. vim.fn.fnameescape(file_path))
+                                    vim.cmd("vsplit " .. vim.fn.fnameescape(path))
                                     vim.cmd("let &splitright = g:splitright_backup")
                                     vim.cmd("unlet g:splitright_backup")
                                 end
@@ -53,63 +58,55 @@ return {
                         },
                     },
                 },
+                pickers = {
+                    find_files = dropdown,
+                    live_grep = dropdown,
+                },
                 extensions = {
                     fzf = {
-                        fuzzy = true,                   -- false will only do exact matching
-                        override_generic_sorter = true, -- override the generic sorter
-                        override_file_sorter = true,    -- override the file sorter
-                        case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
-                        -- the default case_mode is "smart_case"
+                        fuzzy = true,
+                        override_generic_sorter = true,
+                        override_file_sorter = true,
+                        case_mode = "smart_case",
                     },
-                    live_grep_args = {
+                    live_grep_args = vim.tbl_deep_extend("force", dropdown, {
                         auto_quoting = true,
                         mappings = {
                             i = {
                                 ["<M-q>"] = lga_actions.quote_prompt(),
                                 ["<M-i>"] = lga_actions.quote_prompt({ postfix = " -g '*." }),
-                                -- freeze the current list and start a fuzzy search in the frozen list
                                 ["<C-space>"] = lga_actions.to_fuzzy_refine,
                             },
                         },
-                    }
+                    }),
+                    ["ui-select"] = dropdown,
+                    ["telescope-tabs"] = dropdown,
                 },
             })
 
+            -- Load all extensions
+            telescope.load_extension("fzf")
+            telescope.load_extension("live_grep_args")
+            telescope.load_extension("ui-select")
+            telescope.load_extension("telescope-tabs")
+
+            -- Keymaps
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "te", builtin.find_files, {})
-            -- vim.keymap.set("n", "tg", builtin.live_grep, {})
-            vim.keymap.set("n", "tg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+            vim.keymap.set("n", "tg", telescope.extensions.live_grep_args.live_grep_args, {})
             vim.keymap.set("n", "ts", builtin.lsp_document_symbols, {})
             vim.keymap.set("n", "tr", builtin.lsp_references, {})
             vim.keymap.set("n", "tf", builtin.current_buffer_fuzzy_find, {})
             vim.keymap.set("n", "td", builtin.diagnostics, {})
-            vim.keymap.set("n", "tfd", function()
-                require("telescope.builtin").diagnostics({ bufnr = 0 })
-            end, { desc = "Telescope diagnostics for current buffer" })
-
-            -- Keymap to search only in Zig files
+            vim.keymap.set("n", "tfd", function() builtin.diagnostics({ bufnr = 0 }) end, {})
             vim.keymap.set("n", "tz", function()
-                require("telescope.builtin").find_files({
+                builtin.find_files({
                     prompt_title = "Find Zig Files",
                     find_command = { "rg", "--files", "--glob", "*.zig" },
                 })
             end, { desc = "Search only Zig files" })
 
-            require("telescope").load_extension("live_grep_args")
-        end,
-    },
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-        config = function()
-            require("telescope").setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
-                    },
-                },
-            })
-
-            require("telescope").load_extension("ui-select")
+            vim.keymap.set("n", "tt", telescope.extensions["telescope-tabs"].list_tabs, { desc = "Switch Tabs" })
         end,
     },
 }
